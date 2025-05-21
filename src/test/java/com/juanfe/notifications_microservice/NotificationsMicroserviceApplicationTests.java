25dd1b09368a7d406073c8d5fd9956ecdd5ce71f
Juan Felipe Rueda <juan.felipe.rueda@gmail.com>
2025-05-20 20:24:34 -0500

    setting up project

---
+++ src/test/java/com/juanfe/notifications_microservice/NotificationsMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juanfe.notifications_microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationsMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
